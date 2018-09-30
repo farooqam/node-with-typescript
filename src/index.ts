f7690a96106893a99c557c7ec538fb02899fbcbd
Farooq Mahmud <farooq_am@hotmail.com>
2018-09-30 01:57:45 -0700

    Initial commit.

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import app from './app';
+
+const port = process.env.PORT || 5678;
+
+app.listen(port, (err) => {
+    if (err){
+        return console.log(err);
+    }
+
+    return console.log(`Server is listening on port ${port}`);
+});
